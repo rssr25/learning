118fe8d6b45cdcd199983c8cd2bdca73cb0cb1d4
Rahul <rahulsharmacs50@gmail.com>
2023-02-11 17:48:12 +0100

    callbacks

--- streamlit/6_important_concepts.py
+++ streamlit/6_important_concepts.py
@@ -1,9 +1,10 @@
 import streamlit as st
+
+def printer(name):
+    print(name)
 input = st.text_input("Enter your name")
 s_btn = st.button("Submit")
 
 ## Whenever a checkbox is selected, the entire script runs
 if s_btn:
-    opt = st.checkbox("Want to display your name?")
-    if opt:
-        print(input)
+    opt = st.checkbox("Want to display your name?", on_change=printer, args=(input,))
