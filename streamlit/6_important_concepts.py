88f2a3475a731609d1fc81e9c85e6969cf84d163
Rahul <rahulsharmacs50@gmail.com>
2023-02-12 11:48:18 +0100

    session state

--- streamlit/6_important_concepts.py
+++ streamlit/6_important_concepts.py
@@ -8,3 +8,26 @@ s_btn = st.button("Submit")
 ## Whenever a checkbox is selected, the entire script runs
 if s_btn:
     opt = st.checkbox("Want to display your name?", on_change=printer, args=(input,))
+
+#### session states
+#way of sharing variables between reruns for users
+
+text = "😍"
+btn = st.button(text, key='btn1')
+if btn:
+    text="😙"
+#nothing happens when you click, because streamlit reruns.
+#session states allows reserve the values of the variables
+
+text = "😍"
+if "click" not in st.session_state:
+    st.session_state.click = False
+else:
+    if st.session_state.click == False:
+        text="😙"
+        st.session_state.click = True
+    else:
+        text = "😍"
+        st.session_state.click = False
+
+btn = st.button(text)
